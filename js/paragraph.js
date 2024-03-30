7672f73837327061dde305532a9fa69ab88b38e0
Harsh1jatt <pokemonkingdom80@gmail.com>
2024-03-30 17:44:59 +0530

    Initial commit

---
+++ js/paragraph.js
@@ -0,0 +1,4 @@
+const paragraphs = [
+    "A reptant discussion's rest comes with it the thought that the condemned syrup is a wish. The drake of a wallaby becomes a sonant harp. If this was somewhat unclear, spotty ",
+    "Those cowbells are nothing more than elements. This could be, or perhaps before stockings, thoughts were only opinions. A coil of the exclamation is assumed to be a hurtless"
+];
